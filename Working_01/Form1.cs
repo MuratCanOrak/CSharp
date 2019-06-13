5c78ec9013becafad6de224fabfdb695310ac63a
Murat Can ORAK <murat.orak@std.izu.edu.tr>
2019-06-13 17:00:29 +0300

    Değiştirdim

--- Working_01/Form1.cs
+++ Working_01/Form1.cs
@@ -13,7 +13,9 @@ namespace Working_01
     public partial class Form1 : Form
     {
       
-
+        /// <summary>
+        /// /////////////////////////Değiştirdim
+        /// </summary>
             public Form1()
             {
                 InitializeComponent();
@@ -53,7 +55,7 @@ namespace Working_01
                 int numberTwo = Convert.ToInt32(textBox2.Text);
 
                 var sonuc = (numberOne - numberTwo).ToString();
-
+         
                 return sonuc;
             }
             String Carp()
