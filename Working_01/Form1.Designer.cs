18669dff91dd1d85809e5184aa502c03460c5972
Murat Can ORAK <murat.orak@std.izu.edu.tr>
2019-06-13 16:58:21 +0300

    firstCommit

---
+++ Working_01/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Working_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTopla = new System.Windows.Forms.Button();
+            this.btnCikar = new System.Windows.Forms.Button();
+            this.btnCarp = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnTopla
+            // 
+            this.btnTopla.Location = new System.Drawing.Point(413, 246);
+            this.btnTopla.Name = "btnTopla";
+            this.btnTopla.Size = new System.Drawing.Size(80, 48);
+            this.btnTopla.TabIndex = 0;
+            this.btnTopla.Text = "Topla";
+            this.btnTopla.UseVisualStyleBackColor = true;
+            this.btnTopla.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // btnCikar
+            // 
+            this.btnCikar.Location = new System.Drawing.Point(544, 246);
+            this.btnCikar.Name = "btnCikar";
+            this.btnCikar.Size = new System.Drawing.Size(80, 48);
+            this.btnCikar.TabIndex = 1;
+            this.btnCikar.Text = "Çıkar";
+            this.btnCikar.UseVisualStyleBackColor = true;
+            this.btnCikar.Click += new System.EventHandler(this.BtnCikar_Click_1);
+            // 
+            // btnCarp
+            // 
+            this.btnCarp.Location = new System.Drawing.Point(656, 246);
+            this.btnCarp.Name = "btnCarp";
+            this.btnCarp.Size = new System.Drawing.Size(80, 48);
+            this.btnCarp.TabIndex = 2;
+            this.btnCarp.Text = "Çarp";
+            this.btnCarp.UseVisualStyleBackColor = true;
+            this.btnCarp.Click += new System.EventHandler(this.BtnCarp_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(464, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(464, 124);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(618, 201);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnCarp);
+            this.Controls.Add(this.btnCikar);
+            this.Controls.Add(this.btnTopla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTopla;
+        private System.Windows.Forms.Button btnCikar;
+        private System.Windows.Forms.Button btnCarp;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Working_01/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Working_01
+{
+    public partial class Form1 : Form
+    {
+      
+
+            public Form1()
+            {
+                InitializeComponent();
+            }
+
+            private void Form1_Load(object sender, EventArgs e)
+            {
+
+                 MessageBox.Show("Hoş geldiniz.. ");
+            
+            }
+
+            private void Button1_Click(object sender, EventArgs e)
+            {
+
+
+                MessageBox.Show("SONUÇ:  " + Topla());
+                //Topla();
+
+
+            }
+
+            String Topla()
+            {
+
+                int numberOne = Convert.ToInt32(textBox1.Text);
+                int numberTwo = Convert.ToInt32(textBox2.Text);
+
+                var sonuc = (numberOne + numberTwo).ToString();
+
+                return sonuc;
+            }
+            String Cikar()
+            {
+
+                int numberOne = Convert.ToInt32(textBox1.Text);
+                int numberTwo = Convert.ToInt32(textBox2.Text);
+
+                var sonuc = (numberOne - numberTwo).ToString();
+
+                return sonuc;
+            }
+            String Carp()
+            {
+
+                int numberOne = Convert.ToInt32(textBox1.Text);
+                int numberTwo = Convert.ToInt32(textBox2.Text);
+
+                var sonuc = (numberOne * numberTwo).ToString();
+
+                return sonuc;
+            }
+
+         
+
+        private void BtnCikar_Click_1(object sender, EventArgs e)
+        {
+                textBox3.Text = Cikar();
+        }
+
+        private void BtnCarp_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("SONUÇ:  " + Carp());
+        }
+    }
+
+    }
+
+
